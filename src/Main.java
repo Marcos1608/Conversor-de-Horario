10c9bc94933f436eb388ff303b26b19d152514cb
¨Marcos <marcosluis1608@gmail.com>
2026-02-14 17:34:38 -0300

    Projeto de código visando estudar herança e abstração

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+	public static void main(String[] args) {
+		Clock brclock = new BRClock();
+		brclock.setSecond(0);
+		brclock.setMinute(0);
+		brclock.setHour(13);
+		
+		System.out.println(brclock.getTime());
+		
+		System.out.println(new USClock().convert(brclock).getTime());
+	}
+}
